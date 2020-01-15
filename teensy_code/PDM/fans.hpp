a2ba24b7095fdbd80cc2cdd0384c2ef2a02f4380
Dave Yonkers <daveyonks@gmail.com>
2020-01-14 23:28:27 -0500

    update definitions. still need signals

--- teensy_code/PDM/fans.hpp
+++ teensy_code/PDM/fans.hpp
@@ -1,14 +1,17 @@
+// Dave Yonkers, 2020
+
 #ifndef TABLES_HPP
 #define TABLES_HPP
 
 #include <PWMControl.h>
+#include <StateCAN.h>
 
-// Dave Yonkers, 2020
-
-// rows: temp in degrees celcius * 10
-// cols: battery voltage in mV * 10
+// rows: temp in degrees C
+// cols: battery voltage V
 const int fanl_table_rows = 12;
 const int fanl_table_cols = 14;
+const int fanl_table_row_scalar = 10;
+const int fanl_table_col_scalar = 10000;
 int fan_left_table[fanl_table_rows][fanl_table_cols] =
 {
   {    0, 80000, 90000, 100000, 105000, 110000, 119000, 120000, 130000, 137000, 138000, 139000, 142000, 145000},
@@ -29,6 +32,8 @@ int fan_left_table[fanl_table_rows][fanl_table_cols] =
 // cols: battery voltage in mV * 10
 const int fanr_table_rows = 12;
 const int fanr_table_cols = 14;
+const int fanr_table_row_scalar = 10;
+const int fanr_table_col_scalar = 10000;
 int fan_right_table[fanr_table_rows][fanr_table_cols] =
 {
   {    0, 80000, 90000, 100000, 105000, 110000, 119000, 120000, 130000, 137000, 138000, 139000, 142000, 145000},
@@ -45,10 +50,12 @@ int fan_right_table[fanr_table_rows][fanr_table_cols] =
   { 1500,   100,   100,    100,    100,    100,    100,    100,    100,    100,    100,    100,    100,    100}
 };
 
-// rows: temp in degrees celcius * 10
+// rows: temp in degrees celcius
 // cols: RPM
 const int wp_table_rows = 12;
 const int wp_table_cols = 8;
+const int wp_table_row_scalar = 10;
+const int wp_table_col_scalar = 1;
 int wp_table[wp_table_rows][wp_table_cols] =
 {
   {   00,   0,  10,  20, 500, 510,  5000, 15000},
@@ -76,7 +83,9 @@ int fanl_ss_dur = 5000; // duration of soft start in millis
 int fanl_update_freq = 10;
 int fanl_pwm_freq_norm = 40; // normal pwm frequency in Hz
 int fanl_pwm_freq_ss = 420; // soft start pwm frequency in Hz
-PWMDevice fan_left(fanl_pin, fanl_table_rows, fanl_table_cols, fanl_min_pwm, fanl_max_pwm,
+PWMDevice fan_left(fanl_pin, fanl_table_rows, fanl_table_cols,
+                   fanl_table_row_scalar, fanl_table_col_scalar,
+                   ROW_SIG, COL_SIG, fanl_min_pwm, fanl_max_pwm,
                    fanl_ss_dur, fanl_update_freq, fanl_pwm_freq_norm, fanl_pwm_freq_ss);
 
 // right fan definition & partial initialization
@@ -87,8 +96,10 @@ int fanr_ss_dur = 5000; // duration of soft start in millis
 int fanr_update_freq = 10;
 int fanr_pwm_freq_norm = 40; // normal pwm frequency in Hz
 int fanr_pwm_freq_ss = 420; // soft start pwm frequency in Hz
-PWMDevice fan_right(fanr_pin, fanr_table_rows, fanr_table_cols, fanr_min_pwm, fanr_max_pwm,
-                   fanr_ss_dur, fanr_update_freq, fanr_pwm_freq_norm, fanr_pwm_freq_ss);
+PWMDevice fan_right(fanr_pin, fanr_table_rows, fanr_table_cols,
+                    fanr_table_row_scalar, fanr_table_col_scalar,
+                    ROW_SIG, COL_SIG, fanr_min_pwm, fanr_max_pwm,
+                    fanr_ss_dur, fanr_update_freq, fanr_pwm_freq_norm, fanr_pwm_freq_ss);
 
 // water pump definition & partial initialization
 int wp_pin = 1; // update!
@@ -98,7 +109,9 @@ int wp_ss_dur = 5000; // duration of soft start in millis
 int wp_update_freq = 10;
 int wp_pwm_freq_norm = 40; // normal pwm frequency in Hz
 int wp_pwm_freq_ss = 420; // soft start pwm frequency in Hz
-PWMDevice water_pump(wp_pin, wp_table_rows, wp_table_cols, wp_min_pwm, wp_max_pwm,
+PWMDevice water_pump(wp_pin, wp_table_rows, wp_table_cols,
+                     wp_table_row_scalar, wp_table_col_scalar,
+                     ROW_SIG, COL_SIG, wp_min_pwm, wp_max_pwm,
                      wp_ss_dur, wp_update_freq, wp_pwm_freq_norm, wp_pwm_freq_ss);
 
 
