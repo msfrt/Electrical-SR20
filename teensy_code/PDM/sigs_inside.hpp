85d8af46aaad11d2c71b041412cc88ee2897c9a5
Dave Yonkers <daveyonks@gmail.com>
2020-01-16 00:45:35 -0500

    update to reflect changes in user override status

--- teensy_code/PDM/sigs_inside.hpp
+++ teensy_code/PDM/sigs_inside.hpp
@@ -8,4 +8,9 @@ StateSignal M400_engineTemp(16, true, 10, 0, -3276, 3276, 100.0);
 StateSignal M400_rpm(16, true, 1, 0, -3276.8, 3276.7, 0);
 StateSignal M400_batteryVoltage(16, true, 100, 0, -327.68, 327.67, 13.5);
 
+StateSignal USER_brakeLightOverride(16, true, 1, 0, -5, 100, -1, 1000);
+StateSignal USER_fanLeftOverride(16, true, 1, 0, -5, 100, -1, 1000);
+StateSignal USER_fanRightOverride(16, true, 1, 0, -5, 100, -1, 1000);
+StateSignal USER_wpOverride(16, true, 1, 0, -5, 100, -1, 1000);
+
 #endif
