da458d14502473d005ab48ad52ba3b3af52adf97
Dave Yonkers <daveyonks@gmail.com>
2020-01-21 22:35:22 -0500

    include includes

--- teensy_code/ATCC/ATCC.ino
+++ teensy_code/ATCC/ATCC.ino
@@ -1,3 +1,12 @@
+// written by Dave Yonkers, 2020
+
+#include <EasyTimer.h>
+#include <PWMControl.h>
+#include <ReadADC.h>
+#include <StateCAN.h>
+#include <FlexCAN_T4.h>
+#include <SPI.h>
+
 void setup() {
   // put your setup code here, to run once:
 
