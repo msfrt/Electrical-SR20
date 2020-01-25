9ead372a78f21feda19ef1f1c97418a79d3ec51a
Dave Yonkers <daveyonks@gmail.com>
2020-01-24 19:19:27 -0500

    Update sensors.hpp

--- teensy_code/ATCC/sensors.hpp
+++ teensy_code/ATCC/sensors.hpp
@@ -8,11 +8,11 @@
 #define ADC2_CS 9
 #define ADC3_CS 8
 
-ADCSensor sus_travel_fl_sens(ADC3_CS, 3, 0,0); // will need corrected values
-ADCSensor sus_travel_fr_sens(ADC3_CS, 0, 0,0); // (sensor is not chosen yet  ??? nice.)
+ADCSensor sus_travel_fl_sens(ADC3_CS, 3, 0, 1000); // will need corrected values
+ADCSensor sus_travel_fr_sens(ADC3_CS, 0, 0, 1000); // (sensor is not chosen yet  ??? nice.)
 
-ADCSensor brake_temp_fr_sens(ADC1_CS, 0, 0, 0);
-ADCSensor brake_temp_fl_sens(ADC1_CS, 0, 0, 0);
+ADCSensor brake_temp_fr_sens(ADC1_CS, 4, 500, 5);
+ADCSensor brake_temp_fl_sens(ADC1_CS, 7, 500, 5);
 
 ADCSensor brake_pressure_front_sens(ADC2_CS, 1, 0, 0);
 ADCSensor brake_pressure_rear_sens(ADC2_CS, 2, 0, 0);
@@ -29,10 +29,10 @@ ADCSensor tire_temp_fl_outer_sens(ADC2_CS, 6, 0, 0);
 ADCSensor coolant_temp_mid_sens(ADC3_CS, 5, 0, 1); // should return a voltage that will be passed into a bosch temp fcn
 
 
-void sample_ADCs_F(int &ATCC){
+void sample_ADCs_F(){
 
-  static EasyTimer sample_timer_1(1000); // 1,000 Hz - static variable prevents object from deletion when out of scope
-
+  // 1,000 Hz
+  static EasyTimer sample_timer_1(1000); // static variable prevents object from deletion when out of scope
   if (sample_timer_1.isup()){
     sus_travel_fl_sens.sample();
     sus_travel_fr_sens.sample();
