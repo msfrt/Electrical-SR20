c76fd58398b9938eafaee93dc0ea6ba2a437dbd3
kopecnic <44992841+kopecnic@users.noreply.github.com>
2020-01-25 21:43:45 -0500

    Update sensors.hpp

--- teensy_code/PDM/sensors.hpp
+++ teensy_code/PDM/sensors.hpp
@@ -8,22 +8,22 @@
 #define ADC2_CS 10
 
 // ADC1
-ADCSensor keepalive_volt_sens(ADC1_CS, 1, 0, 313);
-ADCSensor       pdm_volt_sens(ADC1_CS, 2, 0, 313);
-ADCSensor      data_volt_sens(ADC1_CS, 3, 0, 313);
-ADCSensor      main_volt_sens(ADC1_CS, 4, 0, 313);
-ADCSensor      fanl_volt_sens(ADC1_CS, 5, 0, 313);
-ADCSensor      fanr_volt_sens(ADC1_CS, 6, 0, 313);
-ADCSensor        wp_volt_sens(ADC1_CS, 7, 0, 313);
-ADCSensor      fuel_volt_sens(ADC1_CS, 8, 0, 313);
+ADCSensor keepalive_volt_sens(ADC1_CS, 0, 0, 313);
+ADCSensor       pdm_volt_sens(ADC1_CS, 1, 0, 313);
+ADCSensor      data_volt_sens(ADC1_CS, 2, 0, 313);
+ADCSensor      main_volt_sens(ADC1_CS, 3, 0, 313);
+ADCSensor      fanl_volt_sens(ADC1_CS, 4, 0, 313);
+ADCSensor      fanr_volt_sens(ADC1_CS, 5, 0, 313);
+ADCSensor        wp_volt_sens(ADC1_CS, 6, 0, 313);
+ADCSensor      fuel_volt_sens(ADC1_CS, 7, 0, 313);
 // ADC2
-ADCSensor    fuel_current_sens(ADC2_CS, 2, 2500, 20);
-ADCSensor      wp_current_sens(ADC2_CS, 3, 2500, 20);
-ADCSensor    fanr_current_sens(ADC2_CS, 4, 2500, 20);
-ADCSensor    fanl_current_sens(ADC2_CS, 5, 2500, 20);
-ADCSensor     pdm_current_sens(ADC2_CS, 6, 2500, 20);
-ADCSensor brakelight_volt_sens(ADC2_CS, 7,    0, 313);
-ADCSensor    starter_volt_sens(ADC2_CS, 8,    0, 313);
+ADCSensor    fuel_current_sens(ADC2_CS, 1, 2500, 20);
+ADCSensor      wp_current_sens(ADC2_CS, 2, 2500, 20);
+ADCSensor    fanr_current_sens(ADC2_CS, 3, 2500, 20);
+ADCSensor    fanl_current_sens(ADC2_CS, 4, 2500, 20);
+ADCSensor     pdm_current_sens(ADC2_CS, 5, 2500, 20);
+ADCSensor brakelight_volt_sens(ADC2_CS, 6,    0, 313);
+ADCSensor    starter_volt_sens(ADC2_CS, 7,    0, 313);
 // stupid board temp
 BoardTemp pdm_board_temp_sens(21, GLO_read_resolution_bits, 22.2222, 187);
 
