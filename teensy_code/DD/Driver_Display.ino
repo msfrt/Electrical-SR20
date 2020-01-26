53e8da0c3dec819d4ee1c02e893966e6104cad97
flipper92017 <59802734+flipper92017@users.noreply.github.com>
2020-01-26 13:33:39 -0500

    Added CAN Messages
    Actually Removed ReadADC, added FlexCAN_T4 and set up CAN Messages.
    testCANX functions added and put in loop().

--- teensy_code/DD/Driver_Display.ino
+++ teensy_code/DD/Driver_Display.ino
@@ -1,5 +1,5 @@
 #include <StateCAN.h>
-#include <ReadADC.h>
+#include <FlexCAN_T4.h>
 #include <EasyTimer.h>
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_GFX.h>
@@ -10,11 +10,21 @@
 //#include <Adafruit_ST7789.h>
 //#include <Adafruit_ST77xx.h>
 
+//NeoPixle Setup
 Adafruit_NeoPixel ledTop (16, 3, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ledLeft (4, 3, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ledRight (4, 3, NEO_GRB + NEO_KHZ800);
 
 EasyTimer ledTimer(1);
+
+//CAN Setup
+FlexCAN_T4<Can1, RX_SIZE_256, TX_SIZE_16> cbus1;
+FlexCAN_T4<Can2, RX_SIZE_256, TX_SIZE_16> cbus2;
+
+StateSignal testCAN1(8, true, 1, 0, -127, 127, 0);
+StateSignal testCAN2(8, true, 1, 0, -127, 127, 0);
+
+static CAN_message_t msg;
 
 int ledCount = 0;
 
@@ -27,6 +37,8 @@ void setup() {
 
 void loop() {
   ledTest();
+  sendCAN1();
+  sendCAN2();
 
 }
 
@@ -51,4 +63,40 @@ void ledTest(){
   ledTop.show();
   ledLeft.show();
   ledRight.show();
+}
+
+void sendCAN1(){
+  msg.id = 410;
+  msg.len = 8;
+
+  testCAN1 = B01101010;
+
+  msg.buf[0] = testCAN1.can_value();
+  msg.buf[1] = 0;
+  msg.buf[2] = 0;
+  msg.buf[3] = 0;
+  msg.buf[4] = 0;
+  msg.buf[5] = 0;
+  msg.buf[6] = 0;
+  msg.buf[7] = 0;
+
+  cbus1.write(msg);
+}
+
+void sendCAN2(){
+  msg.id = 410;
+  msg.len = 8;
+
+  testCAN2 = B10010101;
+
+  msg.buf[0] = testCAN2.can_value();
+  msg.buf[1] = 0;
+  msg.buf[2] = 0;
+  msg.buf[3] = 0;
+  msg.buf[4] = 0;
+  msg.buf[5] = 0;
+  msg.buf[6] = 0;
+  msg.buf[7] = 0;
+
+  cbus2.write(msg);
 }
