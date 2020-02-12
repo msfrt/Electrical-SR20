f03ed7c36fec24c3b46bb3f396562f99b98b8f80
Dave Yonkers <daveyonks@gmail.com>
2020-02-11 23:10:19 -0500

    Include CAN operations (temperature, only, at the moment)

--- teensy_code/TCGPS/TCGPS.ino
+++ teensy_code/TCGPS/TCGPS.ino
@@ -2,20 +2,47 @@
 #include <StateCAN.h>
 #include <FlexCAN_T4.h>
 #include <EasyTimer.h>
+#include <BoardTemp.h>
+
+// can bus decleration
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> cbus1;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> cbus2;
+
+#include "sigs_inside.hpp"
 
 #include "rainbow_pixels.hpp"
+
+#include "can_send.hpp"
+
+// board temp setup
+#define READ_RESOLUTION_BITS 12
+const int board_temp_pin = 19;
+BoardTempDiode board_temp(board_temp_pin, READ_RESOLUTION_BITS, 26.2, 580);
+EasyTimer board_temp_sample_timer(50);
 
 
 // NeoPixel parameters & initialization
 const int pixel_pin = 9;  // teensy pin #
 const int pixel_cnt = 1;  // number of LEDs
       int pixel_brightness_percent = 5;  // 0 - 100%
-
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(pixel_cnt, pixel_pin, NEO_GRB + NEO_KHZ800);
 
 
+EasyTimer debug_timer(10);
+
 
 void setup() {
+
+  // initilize CAN busses
+  cbus1.begin();
+  cbus1.setBaudRate(1000000);
+  cbus2.begin();
+  cbus2.setBaudRate(1000000);
+
+  analogReadResolution(READ_RESOLUTION_BITS);
+  board_temp.begin();
+
+  Serial.begin(115200);
 
   // begin NeoPixel
   pixel.setBrightness(map(pixel_brightness_percent, 0, 100, 0, 255));
@@ -29,5 +56,17 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if (board_temp_sample_timer.isup())
+    board_temp.sample();
+
+
+  if (debug_timer.isup()){
+  }
+
+  // turn the neopixels rainbow colors :-)
   rainbow_pixels(pixel);
+
+  // send can messages
+  send_can1();
+  send_can2();
 }
---
+++ teensy_code/TCGPS/can_send.hpp
@@ -0,0 +1,58 @@
+#ifndef CAN_SEND_DRIVERDISPLAY_HPP
+#define CAN_SEND_DRIVERDISPLAY_HPP
+
+#include <FlexCAN_T4.h>
+#include <EasyTimer.h>
+#include <BoardTemp.h>
+#include "sigs_inside.hpp"
+
+
+static CAN_message_t msg;
+extern BoardTempDiode board_temp;
+
+
+void send_TCGPS_10(){
+  // the following line initializes a counter that is specific to PDM_10, but is static, so it's only created once.
+  static StateCounter ctr;
+
+  // update message ID and length
+  msg.id = 160;
+  msg.len = 8;
+
+  // since the last calculation event, this records the sensor data into the actual signals. The calculations are
+  // done automatically. why don't we simply just throw the sensors into the can message buffer, you ask? Well, the
+  // signals are global and are used elsewhere for other important PDM functions.
+  TCGPS_boardTemp = board_temp.value();
+  TCGPS_teensyTemp = tempmonGetTemp();
+
+
+  // load up the message buffer
+  msg.buf[0] = ctr.value();
+  msg.buf[1] = 0;
+  msg.buf[2] = TCGPS_boardTemp.can_value();
+  msg.buf[3] = TCGPS_boardTemp.can_value() >> 8;
+  msg.buf[4] = TCGPS_teensyTemp.can_value();
+  msg.buf[5] = TCGPS_teensyTemp.can_value() >> 8;
+  msg.buf[6] = 0;
+  msg.buf[7] = 0;
+
+  // send the message
+  cbus2.write(msg);
+}
+
+
+
+void send_can1(){
+  // hey, there's nothing in here! (for now, at least)
+}
+
+void send_can2(){
+
+  static EasyTimer TCGPS_10_timer(10); // 10Hz
+  if (TCGPS_10_timer.isup()){
+    send_TCGPS_10();
+  }
+
+}
+
+#endif
